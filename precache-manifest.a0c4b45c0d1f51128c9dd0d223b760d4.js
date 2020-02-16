106de75afd5113866ceb3f45631a7692d448f2fd
Hasan Patel (NTOSS135) <NTOSS135@tetrapak.com>
2020-02-16 12:38:09 -0500

    Updates

---
+++ precache-manifest.a0c4b45c0d1f51128c9dd0d223b760d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d35cccad3937bab7aba3db2d09459b",
+    "url": "/CatFriends/index.html"
+  },
+  {
+    "revision": "ec36530d5e5e32aeb685",
+    "url": "/CatFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "270642de66ea99869638",
+    "url": "/CatFriends/static/css/main.1836cc6a.chunk.css"
+  },
+  {
+    "revision": "ec36530d5e5e32aeb685",
+    "url": "/CatFriends/static/js/2.859053a3.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/CatFriends/static/js/2.859053a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "270642de66ea99869638",
+    "url": "/CatFriends/static/js/main.5d9f0d68.chunk.js"
+  },
+  {
+    "revision": "5f716148fe44f54d2a05",
+    "url": "/CatFriends/static/js/runtime-main.dba6ffb2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/CatFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
